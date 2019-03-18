cdd0a69ba8249541e769189cb23f0d5b3edd5f30
Jules <jules.comb@gmail.com>
2019-03-18 13:53:21 +0100

    Michel

--- main.js
+++ main.js
@@ -4,9 +4,10 @@ function search() {
     var name = document.getElementById('in').value;
 
     var url = 'https://www.omdbapi.com/?apikey=' + KEY + '&s=' + name + '&r=xml';
+    console.log(encodeURIComponent(url));
     //+ '&type=movie'
 
-    $.getJSON('https://whateverorigin.herokuapp.com/get?url=' + encodeURIComponent(url) + '&callback=?', function(data) {
+    $.getJSON('https://whatever-origin.herokuapp.com/get?url=' + encodeURIComponent(url) + '&callback=?', function(data) {
         init(data.contents);
     });
 }
@@ -15,9 +16,12 @@ function init(xml) {
     var h = document.createElement('xml');
     h.innerHTML = xml;
 
+    console.log(h.innerHTMLw);
+
     var titles = h.getElementsByTagName('result');
 
     for (var i = 0; i < titles.length; i++) {
+        console.log(titles[i].attributes['title'].value.replace(document.getElementById('in').value, 'Jimi Hendrix'));
         document.getElementById('result').innerHTML += titles[i].attributes['title'].value.replace(document.getElementById('in').value, 'Jimi Hendrix') + "<br>";
     }
 }
