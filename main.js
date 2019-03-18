10c1d9c893188d091a8ea8f3b1f8e6c1566b8102
Jules <jules.comb@gmail.com>
2019-03-18 12:17:27 +0100

    Jimi

---
+++ main.js
@@ -0,0 +1,23 @@
+function search() {
+    var KEY = '344e5598';
+
+    var name = document.getElementById('in').value;
+
+    var url = 'http://www.omdbapi.com/?apikey=' + KEY + '&s=' + name + '&r=xml';
+    //+ '&type=movie'
+
+    $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent(url) + '&callback=?', function(data) {
+        init(data.contents);
+    });
+}
+
+function init(xml) {
+    var h = document.createElement('xml');
+    h.innerHTML = xml;
+
+    var titles = h.getElementsByTagName('result');
+
+    for (var i = 0; i < titles.length; i++) {
+        document.getElementById('result').innerHTML += titles[i].attributes['title'].value.replace(document.getElementById('in').value, 'Jimi Hendrix') + "<br>";
+    }
+}
